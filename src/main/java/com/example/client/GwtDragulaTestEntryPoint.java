eac8b8261535a7240f759569c9c84af01775cfe3
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-15 22:11:03 +0100

    Use dragula

--- src/main/java/com/example/client/GwtDragulaTestEntryPoint.java
+++ src/main/java/com/example/client/GwtDragulaTestEntryPoint.java
@@ -1,11 +1,14 @@
 package com.example.client;
 
+import static com.example.client.Document.getElementById;
+import static com.example.client.Dragula.dragula;
+
 import com.google.gwt.core.client.EntryPoint;
 
 public class GwtDragulaTestEntryPoint implements EntryPoint {
 
 	public void onModuleLoad() {
-		Window.alert("Hello");
+		dragula(getElementById("left-defaults"), getElementById("right-defaults"));
 	}
-	
+
 }
