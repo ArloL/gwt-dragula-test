f78f29926ba5b9b5f1836fd277e767a3de7465a1
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-15 23:35:47 +0100

    Add querySelector and querySelectorAll

--- src/main/java/com/example/client/browser/Document.java
+++ src/main/java/com/example/client/browser/Document.java
@@ -8,4 +8,8 @@ public class Document {
 
 	public native static Element getElementById(String id);
 
+	public native static Element querySelector(String selectors);
+
+	public native static NodeList<Element> querySelectorAll(String selectors);
+
 }
