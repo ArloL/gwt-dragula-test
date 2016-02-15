2e267050997a95c35061fa68b522b0e9c225de2c
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-15 22:57:17 +0100

    Add more methods to dragula

--- src/main/java/com/example/client/Dragula.java
+++ src/main/java/com/example/client/Dragula.java
@@ -1,7 +1,9 @@
 package com.example.client;
 
 import jsinterop.annotations.JsMethod;
+import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 @JsType(isNative = true)
@@ -9,5 +11,34 @@ public class Dragula {
 
 	@JsMethod(namespace = JsPackage.GLOBAL)
 	public native static Dragula dragula(Element... elements);
+
+	@JsMethod(namespace = JsPackage.GLOBAL)
+	public native static Dragula dragula(Element[] elements, DragulaOptions options);
+
+	@JsMethod(namespace = JsPackage.GLOBAL)
+	public native static Dragula dragula(DragulaOptions options);
+
+	@JsOverlay
+	public static Dragula dragula(Element element, DragulaOptions options) {
+		return dragula(new Element[] { element }, options);
+	}
+
+	@JsOverlay
+	public static Dragula dragula(Element element, Element element2, DragulaOptions options) {
+		return dragula(new Element[] { element, element2 }, options);
+	}
+
+	@JsOverlay
+	public static Dragula dragula(Element element, Element element2, Element element3, DragulaOptions options) {
+		return dragula(new Element[] { element, element2, element3 }, options);
+	}
+
+	@JsOverlay
+	public static Dragula dragula(DragulaOptions options, Element... elements) {
+		return dragula(elements, options);
+	}
+
+	@JsProperty
+	public native JsArray<Element> getContainers();
 
 }
