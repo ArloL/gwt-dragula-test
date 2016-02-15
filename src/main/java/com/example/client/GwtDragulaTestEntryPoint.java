a29a3638b07bc571c86c78b4ef2ebeac4f303587
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-15 20:54:51 +0100

    Remove unnecessary stuff and rename some things

---
+++ src/main/java/com/example/client/GwtDragulaTestEntryPoint.java
@@ -0,0 +1,12 @@
+package com.example.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class GwtDragulaTestEntryPoint implements EntryPoint {
+
+	public void onModuleLoad() {
+		Window.alert("Hello");
+	}
+	
+}
