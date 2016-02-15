6d78e0bf97072be065bd5f6eed8b1a81e40bdba4
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-15 21:31:31 +0100

    Use own window implementation

--- src/main/java/com/example/client/GwtDragulaTestEntryPoint.java
+++ src/main/java/com/example/client/GwtDragulaTestEntryPoint.java
@@ -1,7 +1,6 @@
 package com.example.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Window;
 
 public class GwtDragulaTestEntryPoint implements EntryPoint {
 
