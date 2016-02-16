d6c508ecd332c37ee9ef6fb6b0742e3915787473
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-16 20:04:56 +0100

    Move index.html to template and serve it

--- src/main/java/com/example/GwtDragulaTestApplication.java
+++ src/main/java/com/example/GwtDragulaTestApplication.java
@@ -3,12 +3,20 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class GwtDragulaTestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GwtDragulaTestApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	String index() {
+		return "index";
 	}
 
 }
